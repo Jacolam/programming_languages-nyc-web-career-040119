179331179634da5503eeb1690e84b5f6bf18165f
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-23 22:46:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,13 +10,13 @@ def reformat_languages(languages)
       type_hash.each do |symbol_type, type_value|
         #binding.pry 
         new_hash[language] = {:type => type_value, :style => []}
-      end
-    end
+  end
+  end
   end
   languages.each do |style, lang_w_type|
       lang_w_type.each do |language, type_hash|
       new_hash[language][:style] << style
-    end
+  end
   end
   #binding.pry
   return new_hash
