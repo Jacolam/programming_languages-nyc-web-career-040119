9832afe9769769ec079bd4f53dd0de92e2f676ba
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-23 22:26:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,13 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash ={}
- languages.each do |style , language |
- language.each do |lang ,type |
+languages.each do |style , language |
+language.each do |lang ,type |
+type.each do |symbol_type , value|
   binding.pry 
+  end 
+   
+ 
  end 
  
  end 
