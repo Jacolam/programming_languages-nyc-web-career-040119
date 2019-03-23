9a0f2a788784cbfe0b8bb5a953e8cdfad20ca910
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-23 22:36:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,19 +2,16 @@ require 'pry'
 
 def reformat_languages(languages)
   # your code here
-  new_hash ={}
-languages.each do |style , language |
-  language.each do |lang ,type |
-    type.each do |symbol_type , value|
-      
-      
-      new_hash[language] = {:type => value , :style => [] }
-     end 
- 
- end 
- end 
- languages.each do |style, data1|
-    data1.each do |language, data2|
+  new_hash = {}
+  languages.each do |style, data1|
+      data1.each do |language, data2|
+      data2.each do |type_category, type|
+        new_hash[language] = {:type => type, :style => []}
+      end
+    end
+  end
+  languages.each do |style, data1|
+      data1.each do |language, data2|
       new_hash[language][:style] << style
     end
   end
