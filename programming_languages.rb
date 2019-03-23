5b3f41a8d522c6e9456113f386e6280be936d9f1
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-23 22:14:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,7 @@ def reformat_languages(languages)
   languages.each do |type, langs|
     langs.each do |lang, lang_data|    
       if !new_hash[lang]
+        binding.pry
         new_hash[lang] = {}
         new_hash[lang][lang_data.keys[0]] = lang_data.values[0]
         new_hash[lang][:style] = []
